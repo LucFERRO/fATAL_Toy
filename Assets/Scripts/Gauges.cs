8aadac9e3119048ee3944d4bb975f293d2b392a6
Azurian-Fire <ay.lavalhki@gmail.com>
2025-03-05 15:09:26 +0100

    Gauges DONE v1

--- Assets/Scripts/Gauges.cs
+++ Assets/Scripts/Gauges.cs
@@ -3,20 +3,39 @@ using UnityEngine;
 
 public class Gauges : MonoBehaviour
 {
-    public int hunterGauge;
+    public int hunterMax = 5;
     public int hunterCurrent;
-    public int villagesGauge;
+    public int hunterIncrement = 1;
+    public int hunterDecrement = 1;
+    public int villagesMax = 7;
     public int villagesCurrent;
-    public int reputationGauge;
+    public int villagesIncrement = 1;
+    public int villagesDecrement = 1;
+    public int reputationMax = 9;
     public int reputationCurrent;
+    public int reputationIncrement = 1;
+    public int reputationDecrement = 1;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Loop entered");
+            hunterCurrent = ClampGauge(hunterCurrent, hunterMax, hunterIncrement);
+            villagesCurrent = ClampGauge(villagesCurrent, villagesMax, villagesIncrement);
+            reputationCurrent = ClampGauge(reputationCurrent, reputationMax, reputationIncrement);
+        }
 
-        };
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Loop entered");
+            hunterCurrent = ClampGauge(hunterCurrent, hunterMax, -hunterDecrement);
+            villagesCurrent = ClampGauge(villagesCurrent, villagesMax, -villagesDecrement);
+            reputationCurrent = ClampGauge(reputationCurrent, reputationCurrent, -reputationIncrement);
+        }
     }
 
-
+    private int ClampGauge(int gaugeValue, int gaugeMax, int incrementValue)
+    {
+        return Mathf.Clamp(gaugeValue + incrementValue, 0, gaugeMax);
+    }
 }
