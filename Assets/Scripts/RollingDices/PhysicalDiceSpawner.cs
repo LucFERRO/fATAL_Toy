a3cdea27a9da2668993b2e5aaacf61dda092f9e6
Luc FERRO <luchenri.ferro@gmail.com>
2025-03-17 18:15:35 +0100

    manual merge 3

---
+++ Assets/Scripts/RollingDices/PhysicalDiceSpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalDiceSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject gameManager;
+    public GameObject diceToSpawn;
+    //public RollingDiceManager diceManager;
+    public int movementSpeed;
+    public float distance;
+    private Vector3 startingPosition;
+    private Vector3 targetPosition;
+    public float positionSwitchTimer;
+    private float timer;
+    void Start()
+    {
+        startingPosition = transform.position;
+        timer = positionSwitchTimer;
+        // x 1.5 - 11.5
+        // z -4 - 2
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = positionSwitchTimer;
+            targetPosition = new Vector3(Random.Range(-distance, distance), 0, Random.Range(-distance, distance)) + startingPosition;
+        }
+        transform.position = Vector3.Slerp(transform.position, targetPosition, movementSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnDice();
+        }
+    }
+
+    public void SpawnDice()
+    {
+        GameObject spawnedDice = Instantiate(diceToSpawn, transform.position, transform.rotation);
+        Vector3 randomSpinVector = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        spawnedDice.GetComponent<Rigidbody>().AddTorque(randomSpinVector.normalized * 5f, ForceMode.Impulse);
+        //diceManager.UpdateDiceData();
+    }
+}
