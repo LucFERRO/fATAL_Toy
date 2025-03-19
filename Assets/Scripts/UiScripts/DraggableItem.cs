2844a94c94aa7ab848d77cd760576822c140f453
Luc FERRO <luchenri.ferro@gmail.com>
2025-03-19 14:10:40 +0100

    y grid WIP + UI rework to heritage

--- Assets/Scripts/UiScripts/DraggableItem.cs
+++ Assets/Scripts/UiScripts/DraggableItem.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Image image;
-    CanvasGroup group;
-    //public Item item;
+    public Image image;
+    public CanvasGroup group;
     public Transform parentAfterDrag;
     public int biomeId;
     public bool isOnDice;
@@ -16,44 +15,34 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         image = GetComponent<Image>();
         group = GetComponent<CanvasGroup>();
-        //item = GetComponent<Item>();
-        //image.sprite = item.sprite;
-
-        //Material potentialMaterialItem = GetComponent<Material>();
-        //Consumable potentialConsumableItem = GetComponent<Consumable>();
-
-        //if (potentialMaterialItem != null || potentialConsumableItem != null)
-        //{
-        //    HandleStackableItems();
-        //}
-
-        //if (item.name == "Material" || item.name == "Consumable")
-        //{
-        //}
     }
 
-    public void HandleStackableItems()
-    {
-        //transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = $"{item.currentStack} / {item.maxStack}";
-    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
-
-        group.alpha = .5f;
+        if (isOnDice)
+        {
+            group.alpha = .5f;
+        }
         image.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = Input.mousePosition;
+        if (isOnDice)
+        {
+            transform.position = Input.mousePosition;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.SetParent(parentAfterDrag);
+        Debug.Log("image local scale " + image.rectTransform.localScale);
+        Debug.Log("local scale " + transform.localScale);
+        image.rectTransform.localScale = Vector3.one;
         group.alpha = 1f;
         image.raycastTarget = true;
     }
--- Assets/Scripts/UiScripts/InventorySlot.cs
+++ Assets/Scripts/UiScripts/InventorySlot.cs
@@ -5,28 +5,40 @@ using JetBrains.Annotations;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    //public Color baseColor;
+
     private void Start()
     {
-        //baseColor = transform.GetComponent<Image>().color;
-        //GetColorFromChild();
-    }
-    //private void OnTransformChildrenChanged()
-    //{
-    //    GetColorFromChild();
-    //}
 
-    //private void GetColorFromChild()
-    //{
-    //    if (transform.childCount != 0)
-    //    {
-    //    }
-    //}
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount != 0)
         {
-            transform.GetChild(0).SetParent(eventData.pointerDrag.GetComponent<DraggableItem>().parentAfterDrag);
+            Transform targetDiceFaceTransform = transform.GetChild(0);
+            bool targetFaceIsOnDice = targetDiceFaceTransform.GetComponent<DraggableItem>().isOnDice;
+            bool heldFaceIsOnDice = eventData.pointerDrag.GetComponent<DraggableItem>().isOnDice;
+
+            if (targetFaceIsOnDice && !heldFaceIsOnDice)
+            {
+                Destroy(targetDiceFaceTransform.gameObject);
+                GameObject createdDiceFace = Instantiate(eventData.pointerDrag, transform);
+                DraggableItem createdDraggableItem = createdDiceFace.GetComponent<DraggableItem>();
+                createdDraggableItem.isOnDice = true;
+                createdDraggableItem.group.alpha = 1f;
+                createdDraggableItem.image.raycastTarget = true;
+                createdDraggableItem.parentAfterDrag = transform;
+                return;
+            }
+            else if (!targetFaceIsOnDice && heldFaceIsOnDice)
+            {
+                Destroy(eventData.pointerDrag);
+                return;
+            }
+            else
+            {
+                targetDiceFaceTransform.SetParent(eventData.pointerDrag.GetComponent<DraggableItem>().parentAfterDrag);
+            }
         }
         eventData.pointerDrag.GetComponent<DraggableItem>().parentAfterDrag = transform;
     }
