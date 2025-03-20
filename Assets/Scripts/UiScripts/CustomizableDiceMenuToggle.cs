e035d8aa6ca8be6b8f772cb11c7acc56326941b6
Luc FERRO <luchenri.ferro@gmail.com>
2025-03-20 17:01:17 +0100

    fix buttons debug + UI inventory key

--- Assets/Scripts/UiScripts/CustomizableDiceMenuToggle.cs
+++ Assets/Scripts/UiScripts/CustomizableDiceMenuToggle.cs
@@ -1,10 +1,24 @@
+using System;
 using UnityEngine;
 
 public class CustomizableDiceMenuToggle : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameManager gameManager;
+    public int uiDistance;
     private Vector3[] startingPositions;
+    private bool isInventoryHidden;
+    private bool IsInventoryHidden
+    {
+        get { return isInventoryHidden; }
+        set
+        {
+            isInventoryHidden = value;
+            HandleInventoryMenu(isInventoryHidden ? uiDistance : 0);
+        }
+    }
+
+
     void Start()
     {
         startingPositions = new Vector3[transform.childCount];
@@ -17,6 +31,23 @@ public class CustomizableDiceMenuToggle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            IsInventoryHidden = !IsInventoryHidden;
+        }
+
+    }
+    private void MoveInventoryUi(GameObject go, Vector3 targetPos)
+    {
+        //go.transform.position = Vector3.Lerp(go.transform.position, targetPos, Time.deltaTime);
+        go.transform.position = targetPos;
+    }
+
+    private void HandleInventoryMenu(int distanceToMove)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MoveInventoryUi(transform.GetChild(i).gameObject, startingPositions[i] + new Vector3(distanceToMove,0,0));
+        }
     }
 }
