4d7514e0ab98c2a6ced6c979a73946bcce9765a8
Luc FERRO <luchenri.ferro@gmail.com>
2025-03-22 14:34:53 +0100

    code clean up post reboot

--- Assets/Scripts/GridCoordinates.cs
+++ Assets/Scripts/GridCoordinates.cs
@@ -2,23 +2,13 @@ using UnityEngine;
 
 public class GridCoordinates : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Vector3Int cellPosition;
-    private HexCoordinates hexCoordinates;
     public string tiletype;
     public GameObject currentPrefab;
 
     void Start()
     {
-        
         Grid grid = transform.parent.GetComponent<Grid>();
         cellPosition = grid.WorldToCell(transform.position);
     }
-
-
-    public Vector3Int HexCoords()
-    {
-        return hexCoordinates.GetHexCoords();
-    }
-
 }
