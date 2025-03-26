a1d3b2ecc25e6c7b904a5ff48f6c9be65f0bb0f5
Luc FERRO <luchenri.ferro@gmail.com>
2025-03-26 13:05:37 +0100

    GetNeighbours function

--- Assets/Scripts/GridCoordinates.cs
+++ Assets/Scripts/GridCoordinates.cs
@@ -5,10 +5,45 @@ public class GridCoordinates : MonoBehaviour
     public Vector3Int cellPosition;
     public string tiletype;
     public GameObject currentPrefab;
+    private Grid grid;
 
     void Start()
     {
-        Grid grid = transform.parent.GetComponent<Grid>();
+        grid = transform.parent.GetComponent<Grid>();
         cellPosition = grid.WorldToCell(transform.position);
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            for (int i = -1; i < 2; i += 2)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    FindNeighbour(new Vector3Int(cellPosition.x + i, cellPosition.y, cellPosition.z + j));
+                }
+            }
+        }
+    }
+
+    private void FindNeighbour(Vector3Int cellCoordinates)
+    {
+        Vector3 coordinates = grid.CellToWorld(cellCoordinates);
+        coordinates.y += 10;
+        GameObject foundObject;
+        Ray ray = new Ray(coordinates, Vector3.down);
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit);
+        if (hit.collider == null)
+        {
+            return;
+        }
+        foundObject = hit.collider.gameObject;
+        Debug.Log(foundObject.name);
+        return;
+        //return foundObject;
     }
 }
