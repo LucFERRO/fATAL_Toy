9b26b2d67e1e7786937a5672d0fa1f549b1faece
Luc FERRO <luchenri.ferro@gmail.com>
2025-05-03 13:08:39 +0200

    autoUnlock comboTiles WIP wrong unlock on UI + autoIncrease maxLockedTiles DONE

--- Assets/Scripts/UnlockManager.cs
+++ Assets/Scripts/UnlockManager.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UnlockManager : MonoBehaviour
 {
     [SerializeField] TileType test;
     string[] tileTypes;
+    private Dictionary<TileType, bool> unlockStatus;
 
     GameManager gameManager;
     [Header("References")]
@@ -28,6 +30,12 @@ public class UnlockManager : MonoBehaviour
         }
 
         tileTypes = Enum.GetNames(typeof(TileType));
+
+        unlockStatus = new Dictionary<TileType, bool>();
+        foreach (TileType tileType in Enum.GetValues(typeof(TileType)))
+        {
+            unlockStatus[tileType] = false;
+        }
     }
 
     // Update is called once per frame
@@ -36,22 +44,37 @@ public class UnlockManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             HandleUnlockComboTile(test);
+        }        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            foreach (var item in unlockStatus)
+            {
+                Debug.Log(item.Key +" "+ item.Value);
+            }
         }
     }
 
-    public void HandleUnlockComboTile(TileType potentialNewCombo)
+    public bool HandleUnlockComboTile(TileType potentialNewCombo)
     {
         for (int i = 0; i < tileTypes.Length; i++)
         {
             if((int)potentialNewCombo < 10)
             {
-                return;
+                return false;
             }
             if (potentialNewCombo.ToString() == tileTypes[i])
             {
                 uiUnlockableTilesItems[i].isAvailable = true;
                 uiUnlockableTilesGO[i].GetComponent<InventorySlot>().EnableInventorySlot();
+
+                if (!unlockStatus[potentialNewCombo])
+                {
+                    unlockStatus[potentialNewCombo] = true;
+                    Debug.Log($"TileType {potentialNewCombo} {i} unlocked.");
+                    return true;
+                }
             }
         }
+        return false;
     }
 }
