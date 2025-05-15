10e012f233e41d6de4a10dbb94c0d29fdce22d7e
Luc FERRO <luchenri.ferro@gmail.com>
2025-05-15 12:21:45 +0200

    diceBounce v2

--- Assets/Scripts/RollingDices/RollingDiceData.cs
+++ Assets/Scripts/RollingDices/RollingDiceData.cs
@@ -106,7 +106,7 @@ public class RollingDiceData : MonoBehaviour
         }
 
         isInUse = true;
-        diceEventInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Dice bouncing");
+        diceEventInstance = FMODUnity.RuntimeManager.CreateInstance("event:/DiceOnTile");
         diceEventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
     }
 
@@ -288,6 +288,7 @@ public class RollingDiceData : MonoBehaviour
             }
 
             diceEventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+            // Timer pour pas skip des notes
             diceEventInstance.setParameterByName("numberOfTiles", numberOfBounces);
             diceEventInstance.start();
             //collision.gameObject.GetComponent<NeighbourTileProcessor>().PlayTileBounceSound(numberOfBounces);
