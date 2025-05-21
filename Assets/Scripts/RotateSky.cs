03050518a1d1e070aea0f80332a91e95bfb7e6df
Luc FERRO <luchenri.ferro@gmail.com>
2025-05-21 12:49:42 +0200

    fix skybox2?

--- Assets/Scripts/RotateSky.cs
+++ Assets/Scripts/RotateSky.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class RotateSky : MonoBehaviour
 {
     public float rotationSpeed = 1.0f;
+    private Material _skybox;
+    private void Start()
+    {
+        _skybox = Instantiate(RenderSettings.skybox);
+        RenderSettings.skybox = _skybox;
+    }
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.timeSinceLevelLoad * rotationSpeed);
+        _skybox.SetFloat("_Rotation", Time.timeSinceLevelLoad * rotationSpeed);
     }
 }
