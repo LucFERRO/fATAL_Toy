775f53754ffa9bc831f864fc26846ec31bb56154
Luc FERRO <luchenri.ferro@gmail.com>
2025-05-21 17:45:41 +0200

    try diceUI hover feedback

--- Assets/Scripts/UiScripts/InventorySlot.cs
+++ Assets/Scripts/UiScripts/InventorySlot.cs
@@ -4,17 +4,7 @@ using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    //private DraggableItem draggableItem;
-    //private Image inventoryImage;
-    //private Image inventoryImageLocked;
-    //private Image biomeImage;
-    //private Image biomeImageLocked;
-    //private GameObject biomeGo;
-    //private GameObject hiddenBiomeGo;
-
-    //public bool isCombo;
-
-    public bool isDicePanelSlot = false; // Determines if this slot belongs to the dice equipment panel
+    public bool isDicePanelSlot;
     public Sprite unlockedBiome;
     public DraggableItem currentlyDraggedItem;
     Animator animator;
@@ -26,6 +16,7 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 
     public void OnMouseOver()
     {
+        Debug.Log(transform.name);
         if (!isDicePanelSlot)
         {
             return;
@@ -36,9 +27,9 @@ public class InventorySlot : MonoBehaviour, IDropHandler
         }
         animator.SetBool("isMouseDraggingOver", true);
     }
-
     public void OnMouseExit()
     {
+        Debug.Log("exited " +transform.name);
         if (!isDicePanelSlot)
         {
             return;
@@ -59,9 +50,8 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 
         if (isDicePanelSlot)
         {
-
             int sameBiomeCount = 0;
-            foreach (Transform child in transform.parent) // Assuming all dice slots share the same parent
+            foreach (Transform child in transform.parent) 
             {
                 DraggableItem childItem = child.GetComponentInChildren<DraggableItem>();
                 if (childItem != null && childItem.biomeId == draggedItem.biomeId)
@@ -76,7 +66,6 @@ public class InventorySlot : MonoBehaviour, IDropHandler
                 //Debug.Log("Cannot add more items with the same biomeId to the dice panel.");
                 return;
             }
-
 
             if (transform.childCount > 0)
             {
@@ -102,72 +91,4 @@ public class InventorySlot : MonoBehaviour, IDropHandler
         }
         draggedItem = null;
     }
-    //private void Start()
-    //{
-    //    draggableItem = transform.GetChild(0).GetComponent<DraggableItem>();
-    //    inventoryImage = GetComponent<Image>();
-    //    biomeImage = transform.GetChild(0).GetComponent<Image>();
-    //    biomeGo = transform.GetChild(0).gameObject;
-    //    if (!draggableItem.isOnDice)
-    //    {
-    //        if (isCombo)
-    //        {
-    //            hiddenBiomeGo = transform.GetChild(1).gameObject;
-    //            //    biomeImageLocked = transform.GetChild(1).GetComponent<Image>();
-    //        }
-    //        EnableInventorySlot();
-    //    }
-    //}
-
-    //public void EnableInventorySlot()
-    //{
-        //    //inventoryImage.enabled = draggableItem.isAvailable;
-        //    //biomeImage.enabled = draggableItem.isAvailable;
-        //    //if (!draggableItem.isOnDice && isCombo)
-        //    //{
-        //    //    //biomeImageLocked.enabled = draggableItem.isAvailable;
-        //    //    //biomeImage.enabled = !draggableItem.isAvailable;
-        //    //    biomeGo.SetActive(draggableItem.isAvailable);
-        //    //    hiddenBiomeGo.SetActive(!draggableItem.isAvailable);
-        //    //}
-    //}
-
-    public void UnlockChildBiomeIcon()
-    {
-        Transform childTransform = transform.GetChild(0);
-        childTransform.GetComponent<Image>().sprite = unlockedBiome;
-        childTransform.GetComponent<DraggableItem>().isDraggable = true;
-    }
-
-    //public void OnDrop(PointerEventData eventData)
-    //{
-    //    if (transform.childCount != 0)
-    //    {
-    //        Transform targetDiceFaceTransform = transform.GetChild(0);
-    //        bool targetFaceIsOnDice = targetDiceFaceTransform.GetComponent<DraggableItem>().isOnDice;
-    //        bool heldFaceIsOnDice = eventData.pointerDrag.GetComponent<DraggableItem>().isOnDice;
-
-    //        if (targetFaceIsOnDice && !heldFaceIsOnDice)
-    //        {
-    //            Destroy(targetDiceFaceTransform.gameObject);
-    //            GameObject createdDiceFace = Instantiate(eventData.pointerDrag, transform);
-    //            DraggableItem createdDraggableItem = createdDiceFace.GetComponent<DraggableItem>();
-    //            createdDraggableItem.isOnDice = true;
-    //            createdDraggableItem.group.alpha = 1f;
-    //            createdDraggableItem.image.raycastTarget = true;
-    //            createdDraggableItem.parentAfterDrag = transform;
-    //            return;
-    //        }
-    //        else if (!targetFaceIsOnDice && heldFaceIsOnDice)
-    //        {
-    //            //Destroy(eventData.pointerDrag);
-    //            return;
-    //        }
-    //        else
-    //        {
-    //            targetDiceFaceTransform.SetParent(eventData.pointerDrag.GetComponent<DraggableItem>().parentAfterDrag);
-    //        }
-    //    }
-    //    eventData.pointerDrag.GetComponent<DraggableItem>().parentAfterDrag = transform;
-    //}
 }
