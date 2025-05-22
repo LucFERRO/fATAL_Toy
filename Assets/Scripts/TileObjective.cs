c1af39156d34fc28daaf3c8b7f7f46ac7a22bd37
Luc FERRO <luchenri.ferro@gmail.com>
2025-05-22 20:15:07 +0200

    trads

--- Assets/Scripts/TileObjective.cs
+++ Assets/Scripts/TileObjective.cs
@@ -17,36 +17,37 @@ public abstract class TileObjective
 
 public class CreateBiomeObjective : TileObjective
 {
-    private TileType biome;
+    private string biome;
 
-    public CreateBiomeObjective(TileType biome, int target)
+    public CreateBiomeObjective(string biome, int target)
     {
         this.biome = biome;
         Target = target;
-        Description = $"Create {Target} {biome}(s)";
+        Description = $"Create {Target} {biome}s";
     }
 
     public override void Evaluate(TileSplitManager manager)
     {
-        Progress = manager.gridTileSplitDictionary.TryGetValue(biome.ToString(), out int count) ? count : 0;
+        Progress = manager.gridTileSplitDictionary.TryGetValue(biome, out int count) ? count : 0;
         IsCompleted = Progress >= Target;
     }
 }
 
 public class CreateComboObjective : TileObjective
 {
-    private TileType combo;
+    private string combo;
 
-    public CreateComboObjective(TileType combo, int target)
+    public CreateComboObjective(string combo, int target)
     {
         this.combo = combo;
         Target = target;
-        Description = $"Create {Target} {combo}(s)";
+        string marshException = combo == "Marshe" ? "Marsh" : combo;
+        Description = $"Create {Target} {marshException}{(target > 1 ? "s" : "")}";
     }
 
     public override void Evaluate(TileSplitManager manager)
     {
-        Progress = manager.comboTileSplitDictionary.TryGetValue(combo.ToString(), out int count) ? count : 0;
+        Progress = manager.comboTileSplitDictionary.TryGetValue(combo, out int count) ? count : 0;
         IsCompleted = Progress >= Target;
     }
 }
@@ -86,13 +87,13 @@ public class ChangeLessTilesInOneRoll : TileObjective
 
 public class AllFacesSameBiomeObjective : TileObjective
 {
-    private TileType biome;
+    private string biome;
 
-    public AllFacesSameBiomeObjective(TileType biome)
+    public AllFacesSameBiomeObjective(string biome)
     {
         this.biome = biome;
         Target = 1;
-        Description = $"Roll a dice with all faces as {biome}";
+        Description = $"Roll a dice with all faces with {biome} elements";
     }
 
     public override void Evaluate(TileSplitManager manager)
@@ -105,9 +106,9 @@ public class AllFacesSameBiomeObjective : TileObjective
 
 public class ChainObjective : TileObjective
 {
-    private TileType biome;
+    private string biome;
 
-    public ChainObjective(TileType biome, int target)
+    public ChainObjective(string biome, int target)
     {
         this.biome = biome;
         Target = target;
@@ -124,6 +125,24 @@ public class ChainObjective : TileObjective
 
 public static class ObjectiveFactory
 {
+    static Dictionary<string, string> tileTypeToString = new Dictionary<string, string>
+    {
+        { "forest", "Forest" },
+        { "lake", "Lake" },
+        { "mountain", "Mountain" },
+        { "plain", "Plain" },
+        { "forestForest", "Thicket" },
+        { "forestLake", "Marshe" },
+        { "forestMountain", "Highland" },
+        { "forestPlain", "Glade" },
+        { "lakeLake", "Basin" },
+        { "lakeMountain", "Caldera" },
+        { "lakePlain", "Bog" },
+        { "mountainMountain", "Summit" },
+        { "mountainPlain", "Plateau" },
+        { "plainPlain", "Cropfield" }
+    };
+
     public static TileObjective GenerateRandomObjective(TileSplitManager manager)
     {
         int type = UnityEngine.Random.Range(0, 5);
@@ -131,22 +150,26 @@ public static class ObjectiveFactory
         {
             case 0:
                 string biome = manager.gridTileSplitDictionary.Keys.ElementAt(UnityEngine.Random.Range(0, manager.gridTileSplitDictionary.Count));
-                return new CreateBiomeObjective((TileType)System.Enum.Parse(typeof(TileType), biome), UnityEngine.Random.Range(5, 15));
+                return new CreateBiomeObjective(tileTypeToString[biome], UnityEngine.Random.Range(5, 15));
             case 1:
                 string combo = manager.comboTileSplitDictionary.Keys.ElementAt(UnityEngine.Random.Range(0, manager.comboTileSplitDictionary.Count));
-                return new CreateComboObjective((TileType)System.Enum.Parse(typeof(TileType), combo), UnityEngine.Random.Range(2, 6));
+                return new CreateComboObjective(tileTypeToString[combo], UnityEngine.Random.Range(2, 6));
             case 2:
                 return new ChangeTilesInOneRoll(UnityEngine.Random.Range(4, 10));            
             case 3:
                 return new ChangeLessTilesInOneRoll(UnityEngine.Random.Range(2, 4));
             case 4:
                 string allFaceBiome = manager.gridTileSplitDictionary.Keys.ElementAt(UnityEngine.Random.Range(0, manager.gridTileSplitDictionary.Count));
-                return new AllFacesSameBiomeObjective((TileType)System.Enum.Parse(typeof(TileType), allFaceBiome));
+                return new AllFacesSameBiomeObjective(tileTypeToString[allFaceBiome]);
             //case 5:
             //    string chainBiome = manager.gridTileSplitDictionary.Keys.ElementAt(UnityEngine.Random.Range(0, manager.gridTileSplitDictionary.Count));
             //    return new ChainObjective((TileType)System.Enum.Parse(typeof(TileType), chainBiome), UnityEngine.Random.Range(3, 8));
             default:
                 return null;
         }
+    }
+    static string ConvertToString(TileType tileType)
+    {
+        return tileType.ToString();
     }
 }
